448f1943ed641278161d60fd0668ca40bf0f8a5a
Pompe <Pompedev@gmail.com>
2021-12-01 17:44:23 +0100

    First commit. Solved day1

---
+++ AOC21/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace AOC21 {
+    internal static class Program {
+        private static void Main(string[] args) {
+            
+            var content = File.ReadAllLines(@"C:\temp\1_1.txt");
+            Console.WriteLine(new SolveDay1().Solve1(content));
+            
+        }
+    }
+}
---
+++ AOC21/SolveDay1.cs
@@ -0,0 +1,24 @@
+namespace AOC21 {
+    class SolveDay1 {
+        public int Solve1(string[] content) {
+            var count = 0;
+
+            for (int i = 3; i < content.Length; i++) {
+                var fourth = int.Parse(content[i]);
+                var third = int.Parse(content[i - 1]);
+                var second = int.Parse(content[i - 2]);
+                var first = int.Parse(content[i - 3]);
+
+                var start = first + second + third;
+                var stop = second + third + fourth;
+                
+                
+                if (stop > start) {
+                    count++;
+                } 
+            }
+
+            return count;
+        }
+    }
+}
